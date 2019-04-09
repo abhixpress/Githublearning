4cbfe89feacbc95e89aad47c496e6b1563afcb6a
Abhinav shankar <shankarabhinav26@gmail.com>
2019-04-09 13:11:06 +0530

    First Commint

---
+++ StaticMethod/src/com/abhinav/util/Test.java
@@ -0,0 +1,11 @@
+package com.abhinav.util;
+public class Test {
+ public static void main(String[]args) {
+	 
+	 String s1 = new String("Hello");  
+	 String s2 = new String("Hello");
+	 System.out.println(s1);
+	 System.out.println(s2);
+}
+}
+
